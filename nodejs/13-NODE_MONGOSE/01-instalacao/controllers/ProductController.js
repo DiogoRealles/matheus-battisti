7257edd8e8804ad719b7ebb4db53a7ac9749819a
Diogo Realles <diogorealles@hotmail.com>
2024-06-25 21:08:02 -0300

    [FEATURE] NodeJS + Mongoose

--- nodejs/13-NODE_MONGOSE/01-instalacao/controllers/ProductController.js
+++ nodejs/13-NODE_MONGOSE/01-instalacao/controllers/ProductController.js
@@ -4,8 +4,6 @@ module.exports = class ProductController {
   static async showProducts(req, res) {
     const products = await Product.find().lean();
 
-    //console.log(products)
-
     res.render('products/all', { products });
   }
 
@@ -31,29 +29,29 @@ module.exports = class ProductController {
     res.render('products/product', { product });
   }
 
-  // static async removeProduct(req, res) {
-  //   const { id } = req.params;
+  static async removeProduct(req, res) {
+    const { id } = req.params;
 
-  //   await Product.removeProductById(id);
+    await Product.deleteOne({ _id: id });
 
-  //   res.redirect('/products');
-  // }
+    res.redirect('/products');
+  }
 
   static async editProduct(req, res) {
     const { id } = req.params;
 
-    const product = await Product.getProductById(id);
+    const product = await Product.findById(id).lean();
 
     res.render('products/edit', { product });
   }
 
-  // static async editProductPost(req, res) {
-  //   const { id, name, image, price, description } = req.body;
+  static async editProductPost(req, res) {
+    const { id, name, image, price, description } = req.body;
 
-  //   const product = new Product(name, image, price, description);
+    const product = { id, name, image, price, description };
 
-  //   await product.updateProduct(id);
+    await Product.updateOne({ _id: id }, product);
 
-  //   res.redirect('/products');
-  // }
+    res.redirect('/products');
+  }
 };
