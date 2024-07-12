f00d5649e161105714f3312c73fc825587de037a
Diogo Realles <diogorealles@hotmail.com>
2024-07-12 03:17:25 -0300

    [UPDATE] ...

--- nodejs/15-GET-A-PET/backend/controllers/UserController.js
+++ nodejs/15-GET-A-PET/backend/controllers/UserController.js
@@ -1,5 +1,8 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const createUserToken = require('../helpers/create-user-token');
+const getToken = require('../helpers/get-token');
 
 module.exports = class UserController {
   static async register(req, res) {
@@ -56,12 +59,72 @@ module.exports = class UserController {
 
     try {
       const newUser = await user.save();
-      res.status(201).json({
-        message: 'Usuário criado!',
-        newUser,
-      });
+      await createUserToken(newUser, req, res);
     } catch (error) {
       res.status(500).json({ message: error });
+    }
+  }
+
+  static async login(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      res.status(422).json({ message: 'O email é obrigatório!' });
+      return;
+    }
+
+    if (!password) {
+      res.status(422).json({ message: 'A senha é obrigatória!' });
+      return;
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      res
+        .status(422)
+        .json({ message: 'Não há usuário cadastrado com esse email!' });
+      return;
+    }
+
+    const checkPassword = await bcrypt.compare(password, user.password);
+
+    if (!checkPassword) {
+      res.status(422).json({ message: 'Verifique a senha e tente novamente!' });
+      return;
+    }
+
+    await createUserToken(user, req, res);
+  }
+
+  static async checkUser(req, res) {
+    let currentUser;
+
+    console.log(req.headers.authorization);
+    if (req.headers.authorization) {
+      const token = getToken(req);
+
+      const decoded = jwt.verify(token, 'nossosecret');
+
+      currentUser = await User.findById(decoded.id);
+
+      currentUser.password = undefined;
+    } else {
+      currentUser = null;
+    }
+
+    res.status(200).send(currentUser);
+  }
+
+  static async getUserById(req, res) {
+    const { id } = req.params;
+
+    try {
+      const user = await User.findById(id).select('-password');
+
+      res.status(200).json({ user });
+    } catch (error) {
+      return res.status(422).json({ message: 'Usuário não encontrado!' });
     }
   }
 };
